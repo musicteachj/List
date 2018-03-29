a289a0eacc9faa84a38c3bf0fbf57e3587efd39f
musicteachj <jimilittle3@hotmail.com>
2018-03-29 17:12:19 -0400

    setup structure
    add navbar, form(without form tag!), add table

--- script/script.js
+++ script/script.js
@@ -1,1 +1,15 @@
-alert('Hello World');
+console.log('Hello World');
+
+document.getElementById("button").onclick = function() {
+	var firstName = "";
+
+	firstName = document.getElementById("fName").value;
+
+	console.log(firstName);
+	alert(firstName);
+	document.getElementById('para').innerHTML = firstName;
+	document.getElementById("fName").value = "";
+	document.getElementById("fName").style.backgroundColor = "white";
+}
+
+
